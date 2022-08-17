7cc886e0b8746e9be68f96fd0f7b729d4f747ee2
Tanveer Karim <tanveer.karim@cfa.harvard.edu>
2022-08-17 15:44:34 -0400

    finalized tests

--- gspice/tests/test_specUtils.py
+++ gspice/tests/test_specUtils.py
@@ -36,15 +36,15 @@ class TestStandardScale(unittest.TestCase):
                                    desired = expected_output, 
                                    atol = atol, rtol = rtol)
 
-# class TestCovar(unittest.TestCase):
-#     def test_data(self):
-#         naive_cov, _ = covar(spec, checkmean=True)
-#         test_output = np.mean(naive_cov)
-#         expected_output = 1095.4699138
+class TestCovar(unittest.TestCase):
+    def test_data(self):
+        naive_cov, _ = covar(spec, checkmean=True)
+        test_output = np.mean(naive_cov)
+        expected_output = 1095.4699138
         
-#         np.testing.assert_allclose(actual = test_output,
-#                                    desired = expected_output,
-#                                    atol = atol, rtol = rtol)
+        np.testing.assert_allclose(actual = test_output,
+                                   desired = expected_output,
+                                   atol = atol, rtol = rtol)
 
 class TestGaussianEstimate(unittest.TestCase):
     #arbitrary masking
@@ -88,66 +88,66 @@ class TestGaussianEstimate(unittest.TestCase):
                                    desired = self.predcovar,
                                    atol = atol, rtol = rtol)
 
-# class TestGPInterp(unittest.TestCase):
+class TestGPInterp(unittest.TestCase):
 
-#     pred, predvar = gp_interp(Dvec, covmat, nguard = 20)
+    pred, predvar = gp_interp(Dvec, covmat, nguard = 20)
 
-#     def test_pred(self):
-#         test_output = np.std(self.pred, ddof = 1)
-#         expected_output = 15.412908264176547
+    def test_pred(self):
+        test_output = np.std(self.pred, ddof = 1)
+        expected_output = 15.412908264176547
 
-#         np.testing.assert_allclose(actual = test_output, 
-#                                    desired = expected_output,
-#                                    atol = 1e-4, rtol = 1e-5)
+        np.testing.assert_allclose(actual = test_output, 
+                                   desired = expected_output,
+                                   atol = atol, rtol = rtol)
 
-#     def test_predvar(self):
-#         test_output = np.std(self.predvar, ddof = 1)
-#         expected_output = 28.429495082182775
+    def test_predvar(self):
+        test_output = np.std(self.predvar, ddof = 1)
+        expected_output = 28.429495082182775
 
-#         np.testing.assert_allclose(actual = test_output, 
-#                                    desired = expected_output,
-#                                    atol = atol, rtol = rtol)
+        np.testing.assert_allclose(actual = test_output, 
+                                   desired = expected_output,
+                                   atol = atol, rtol = rtol)
 
-#     def test_chi(self):
-#         test_output = np.std((Dvec - self.pred)/np.sqrt(self.predvar), ddof = 1)
-#         print(test_output)
-#         expected_output = 0.99990003600975130
+    def test_chi(self):
+        test_output = np.std((Dvec - self.pred)/np.sqrt(self.predvar), ddof = 1)
+        print(test_output)
+        expected_output = 0.99990003600975130
 
-#         np.testing.assert_allclose(actual = test_output, 
-#                                    desired = expected_output,
-#                                    atol = atol, rtol = rtol)
+        np.testing.assert_allclose(actual = test_output, 
+                                   desired = expected_output,
+                                   atol = atol, rtol = rtol)
 
-# class TestGetChimask(unittest.TestCase):
-#     def test_data(self):
-#         chimask = get_chimask(spec, ivar, mask != 0, 20)
-#         test_output = np.sum(chimask)
-#         print(test_output)
-#         expected_output = 444
+class TestGetChimask(unittest.TestCase):
+    def test_data(self):
+        chimask = get_chimask(spec, ivar, mask != 0, 20)
+        test_output = np.sum(chimask)
+        print(test_output)
+        expected_output = 444
 
-#         np.testing.assert_allclose(actual = test_output,
-#                                    desired = expected_output,
-#                                   atol = atol, rtol = rtol)
+        np.testing.assert_allclose(actual = test_output,
+                                   desired = expected_output,
+                                  atol = atol, rtol = rtol)
 
-# class TestCovarIterMask(unittest.TestCase):
-#     covmat_iter, finalmask = covar_iter_mask(spec, ivar, mask, nsigma=[20,8,6], maxbadpix=64)
-#     print('stds ', covmat.std(ddof = 1))
-#     print('stds ', finalmask.std(ddof = 1))
+class TestCovarIterMask(unittest.TestCase):
+    covmat_iter, finalmask = covar_iter_mask(spec, ivar, mask, nsigma=[20,8,6], maxbadpix=64)
+    print('stds ', covmat.std(ddof = 1))
+    print('stds ', finalmask.std(ddof = 1))
     
-#     def test_covmat(self):
-#         expected_output = np.std(cov_idl, ddof =1)
-#         test_output = np.std(self.covmat_iter, ddof =1)
+    def test_covmat(self):
+        expected_output = np.std(cov_idl, ddof =1)
+        test_output = np.std(self.covmat_iter, ddof =1)
         
-#         np.testing.assert_allclose(actual = test_output,
-#                                    desired = expected_output,
-#                                   atol = 1e-4, rtol = 1e-6) #
+        np.testing.assert_allclose(actual = test_output,
+                                   desired = expected_output,
+                                  atol = atol, rtol = rtol) #
 
-#     def test_finalmask(self):
-#         expected_output = finalmask_idl.std(ddof = 1)
-#         test_output = np.std(self.finalmask, ddof  = 1)
+    def test_finalmask(self):
+        expected_output = finalmask_idl.std(ddof = 1)
+        test_output = np.std(self.finalmask, ddof  = 1)
 
-#         np.testing.assert_allclose(actual = test_output,
-#                                    desired = expected_output,
-#                                   atol = 1e-5, rtol = 1e-4)
+        np.testing.assert_allclose(actual = test_output,
+                                   desired = expected_output,
+                                  atol = atol, rtol = rtol)
             
 if __name__ == '__main__':
      unittest.main()
